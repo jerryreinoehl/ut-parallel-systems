1e8fbad9f1d6414914a41830d8f2cdd4ee79340d
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-09-22 13:40:12 -0500

    Parse input

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,7 +1,54 @@
+#include "main.h"
 #include "args.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
 int main(int argc, char *argv[]) {
   KmeansArgs args{argc, argv};
+  std::vector<std::vector<double>> vectors;
+
+  vectors = read_points(args.input_file, args.num_dims);
 
   return 0;
+}
+
+std::vector<std::vector<double>> read_points(const std::string& filename, int dim) {
+  std::ifstream in{filename};
+  int num_points;
+  std::string line;
+  std::stringstream ss;
+  int idx;
+
+  std::getline(in, line);
+  ss = std::stringstream(line);
+  ss >> num_points;
+
+  std::vector<std::vector<double>> vectors;
+  vectors.reserve(num_points);
+
+  while (!in.eof()) {
+    std::vector<double> vect;
+    double num;
+
+    vect.reserve(dim);
+
+    std::getline(in, line);
+    if (line == "")
+      continue;
+
+    ss = std::stringstream(line);
+    ss >> idx;
+
+    for (int i = 0; i < dim; i++) {
+      ss >> num;
+      vect.push_back(num);
+    }
+
+    vectors.push_back(vect);
+  }
+
+  return vectors;
 }
---
+++ lab2/src/main.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+std::vector<std::vector<double>> read_points(const std::string& filename, int dim);
