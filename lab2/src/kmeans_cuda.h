5e58a782c756f1b25e2265a65550657753ea993a
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-09-23 15:10:56 -0500

    Create cudaptr smart pointer

--- lab2/src/kmeans_cuda.h
+++ lab2/src/kmeans_cuda.h
@@ -1,7 +1,9 @@
+// vim: ft=cuda
 #pragma once
 
 #include "args.h"
 #include <memory>
+#include <cuda_runtime_api.h>
 
 void kmeans_cuda(
   const KmeansArgs& args,
@@ -12,3 +14,34 @@ void kmeans_cuda(
   int *num_iters,
   double *time_ms
 );
+
+// Simple CUDA smart pointer.
+template <typename T>
+class cudaptr {
+  public:
+    cudaptr static make(size_t size) {
+      T *ptr;
+
+      cudaError_t err = cudaMalloc(&ptr, size * sizeof(T));
+
+      if (err != cudaSuccess) {
+        printf("%s\n", cudaGetErrorString(err));
+        exit(1);
+      }
+
+      return {ptr};
+    }
+
+    cudaptr(T *t) : data_(t) {};
+
+    ~cudaptr() {
+      cudaFree(data_);
+    };
+
+    T *get() {
+      return data_;
+    }
+
+  private:
+    T *data_;
+};
