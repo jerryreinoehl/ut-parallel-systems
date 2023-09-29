6db1561d5ed118d35dd76bdb74640a2305d1f114
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-09-28 21:32:53 -0500

    Implement hostptr<T>

--- lab2/src/cudaptr.h
+++ lab2/src/cudaptr.h
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include "cudaarena.h"
 
+template <typename T>
+class hostptr;
+
 // CUDA smart pointer.
 template <typename T>
 class cudaptr {
@@ -54,8 +57,14 @@ class cudaptr {
     }
 
     // Allocate memory on the device of size `size * sizeof(T)` and initialize
+    // with memory from host pointer `ptr.get()`.
+    cudaptr static make_from(const hostptr<T>& ptr, size_t size) {
+      return cudaptr::make_from(ptr.get(), size);
+    }
+
+    // Allocate memory on the device of size `size * sizeof(T)` and initialize
     // with memory from host pointer `*data`.
-    cudaptr static make_from(CudaArena& arena, T *data, size_t size) {
+    cudaptr static make_from(CudaArena& arena, const T *data, size_t size) {
       auto ptr = cudaptr::make(arena, size);
       cudaError_t err = cudaMemcpy(
         ptr.get(), data, size * sizeof(T), cudaMemcpyHostToDevice
@@ -72,6 +81,12 @@ class cudaptr {
     // Allocate memory on the device of size `size * sizeof(T)` and initialize
     // with memory from host pointer `ptr.get()`.
     cudaptr static make_from(CudaArena& arena, const std::unique_ptr<T[]>& ptr, size_t size) {
+      return cudaptr::make_from(arena, ptr.get(), size);
+    }
+
+    // Allocate memory on the device of size `size * sizeof(T)` and initialize
+    // with memory from host pointer `ptr.get()`.
+    cudaptr static make_from(CudaArena& arena, const hostptr<T>& ptr, size_t size) {
       return cudaptr::make_from(arena, ptr.get(), size);
     }
 
@@ -128,6 +143,12 @@ class cudaptr {
       to_host(ptr.get());
     }
 
+    // Copy device memory to host memory. `ptr` should point to a region
+    // with at least the same size of memory allocated by this cudaptr.
+    void to_host(hostptr<T>& ptr) const {
+      to_host(ptr.get());
+    }
+
     // Copy host memory to device memory.
     void from_host(T *host) {
       cudaError_t err;
@@ -171,3 +192,50 @@ class cudaptr {
     size_t size_;
 };
 
+template <typename T>
+class hostptr {
+  public:
+    // Allocate pinned memory on the host of size `size * sizeof(T)`.
+    hostptr static make(size_t size) {
+      T *ptr;
+
+      cudaError_t err = cudaMallocHost((void**)&ptr, size * sizeof(T));
+
+      if (err != cudaSuccess) {
+        printf("%s\n", cudaGetErrorString(err));
+        exit(1);
+      }
+
+      return {ptr, size};
+    }
+
+    hostptr(T *t, size_t size) : data_(t), size_(size) {};
+    hostptr(void *t, size_t size) : data_(static_cast<T*>(t)), size_(size) {};
+    hostptr(hostptr&& other) : data_(other.data_), size_(other.size_) {
+      other.data_ = nullptr;
+      other.size_ = 0;
+    }
+
+    ~hostptr() {
+      cudaFreeHost(data_);
+    };
+
+    T *get() {
+      return data_;
+    }
+
+    const T *get() const {
+      return data_;
+    }
+
+    T& operator[](int i) {
+      return data_[i];
+    }
+
+    const T& operator[](int i) const {
+      return data_[i];
+    }
+  private:
+    T *data_;
+    size_t size_;
+};
