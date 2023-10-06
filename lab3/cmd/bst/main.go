e76c00684217f2a7b989de59c50a2c489ff84cd5
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-10-06 11:02:59 -0500

    Use generic btree

--- lab3/cmd/bst/main.go
+++ lab3/cmd/bst/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	bt := btree.NewBTree()
+	bt := btree.NewBTree[int]()
 	bt.Insert(10, 23, 11, 5)
 	bt.Insert(10)
 
@@ -24,8 +24,8 @@ func main() {
 	}
 }
 
-func loadTrees(file string) []*btree.BTree {
-	trees := make([]*btree.BTree, 0)
+func loadTrees(file string) []*btree.BTree[int] {
+	trees := make([]*btree.BTree[int], 0)
 
 	f, err := os.Open(file)
 	if err != nil {
@@ -37,18 +37,17 @@ func loadTrees(file string) []*btree.BTree {
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
-		tree := btree.NewBTree()
+		tree := btree.NewBTree[int]()
 		trees = append(trees, &tree)
 
 		line := scanner.Text()
-		fmt.Println(line)
 
 		for _, item := range strings.Split(line, " ") {
 			parsed, err := strconv.Atoi(item)
 			if err != nil {
 				log.Fatal(err)
 			}
-			tree.Insert(btree.Item(parsed))
+			tree.Insert(parsed)
 		}
 	}
 
