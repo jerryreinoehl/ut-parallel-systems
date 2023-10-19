3d47020e97c13818ec38d032ecea50c09ac7c85f
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-10-18 19:36:55 -0500

    Add rwmutex to HashTable

--- lab3/pkg/hashtable/hashtable.go
+++ lab3/pkg/hashtable/hashtable.go
@@ -12,13 +12,13 @@ type row[V any] struct {
 }
 
 type HashTable[K hashable, V any] struct {
-	mu sync.Mutex
+	mu sync.RWMutex
 	data map[K]*row[V]
 }
 
 func NewHashTable[K hashable, V any] () *HashTable[K, V] {
 	return &HashTable[K, V]{
-		mu: sync.Mutex{},
+		mu: sync.RWMutex{},
 		data: make(map[K]*row[V]),
 	}
 }
@@ -31,28 +31,37 @@ func newRow[V any]() *row[V] {
 }
 
 func (ht *HashTable[K, V]) Put(k K, v V) {
+	var row *row[V]
+
+	ht.mu.RLock()
 	// Check if we need to create a new row.
 	if ht.data[k] == nil {
+		ht.mu.RUnlock()
 		ht.mu.Lock()
 		// We must recheck in case this row was created before we acquired the
-		// table lock.
+		// write lock.
 		if ht.data[k] == nil {
 			ht.data[k] = newRow[V]()
 		}
+		row = ht.data[k]
 		ht.mu.Unlock()
+	} else {
+		row = ht.data[k]
+		ht.mu.RUnlock()
 	}
 
 	// Acquire lock for row.
-	row := ht.data[k]
 	row.mu.Lock()
 	row.data = append(row.data, v)
 	row.mu.Unlock()
 }
 
 func (ht *HashTable[K, V]) Get(k K) []V {
+	ht.mu.RLock()
 	if ht.data[k] == nil {
 		return nil
 	}
+	ht.mu.RUnlock()
 
 	row := ht.data[k]
 	row.mu.Lock()
@@ -64,8 +73,12 @@ func (ht *HashTable[K, V]) Get(k K) []V {
 
 func (ht *HashTable[K, V]) Keys() []K {
 	keys := make([]K, 0, 32)
+
+	ht.mu.RLock()
 	for k := range ht.data {
 		keys = append(keys, k)
 	}
+	ht.mu.RUnlock()
+
 	return keys
 }
