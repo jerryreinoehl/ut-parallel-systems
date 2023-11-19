f30b7f68a0dad19d302605d7ccfad9a09508ea7e
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-11-19 15:25:32 -0600

    Write particles to file

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -4,6 +4,7 @@
 
 #include <fstream>
 #include <sstream>
+#include <iomanip>
 
 int main(int argc, char **argv) {
   Args args{argc, argv};
@@ -40,4 +41,17 @@ std::vector<Particle> read_particles(const std::string& filename) {
   }
 
   return particles;
+}
+
+void write_particles(const std::string& filename, const std::vector<Particle>& particles) {
+  std::ofstream out{filename};
+
+  out << std::scientific << std::setprecision(6);
+
+  out << particles.size() << '\n';
+
+  for (const auto& particle : particles) {
+    out << particle.id() << '\t' << particle.x() << '\t' << particle.y() << '\t'
+        << particle.mass() << '\t' << particle.dx() << '\t' << particle.dy() << '\n';
+  }
 }
--- lab5/src/main.h
+++ lab5/src/main.h
@@ -6,3 +6,4 @@
 #include <vector>
 
 std::vector<Particle> read_particles(const std::string& filename);
+void write_particles(const std::string& filename, const std::vector<Particle>& particles);
