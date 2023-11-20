1a0b12f431b85e5b5fada775a1751f51bc3c4bad
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-11-20 11:59:51 -0600

    Add visualization

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -10,12 +10,18 @@
 
 int main(int argc, char **argv) {
   Args args{argc, argv};
-  seq_barnes_hut(args);
+  GLFWwindow *window{};
+
+  if (args.visual()) {
+    window = init_window();
+  }
+
+  seq_barnes_hut(args, window);
 
   return 0;
 }
 
-void seq_barnes_hut(const Args& args) {
+void seq_barnes_hut(const Args& args, GLFWwindow *window) {
   const double size = 4;
   double gravity = args.gravity();
   double threshold = args.threshold();
@@ -29,6 +35,10 @@ void seq_barnes_hut(const Args& args) {
   clock_t start = std::clock(), end;
 
   for (int step = 0; step < args.steps(); step++) {
+    if (args.visual()) {
+      draw(window, particles);
+    }
+
     spt.reset();
     spt.put(particles);
     spt.compute_centers();
@@ -86,4 +96,82 @@ void write_particles(const std::string& filename, const std::vector<Particle>& p
     out << particle.id() << '\t' << particle.x() << '\t' << particle.y() << '\t'
         << particle.mass() << '\t' << particle.dx() << '\t' << particle.dy() << '\n';
   }
+}
+
+GLFWwindow *init_window() {
+  const int width = 600, height = 600;
+  GLFWwindow *window;
+
+  if (!glfwInit()) {
+    fprintf(stderr, "Failed to initialize GLFW\n");
+    return nullptr;
+  }
+
+  // Open a window and create is OpenGL context.
+  window = glfwCreateWindow(width, height, "Simulation", NULL, NULL);
+  if (window == NULL) {
+    fprintf(stderr, "Failed to open GLFW window.\n");
+    glfwTerminate();
+    return nullptr;
+  }
+
+  glfwMakeContextCurrent(window); // Initialize GLEW
+  if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW\n");
+    return nullptr;
+  }
+
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+  return window;
+}
+
+void draw(GLFWwindow *window, const std::vector<Particle>& particles) {
+  glClear(GL_COLOR_BUFFER_BIT);
+  drawOctreeBounds2D();
+
+  for (const auto& particle : particles) {
+    drawParticle2D(particle);
+  }
+
+  // Swap buffers
+  glfwSwapBuffers(window);
+  glfwPollEvents();
+}
+
+void drawOctreeBounds2D() {
+  glBegin(GL_LINES);
+
+  // Set the color of lines to be white.
+  glColor3f(1.0f, 1.0f, 1.0f);
+
+  // Specify the start point's coordinates.
+  glVertex2f(-1, 0);
+
+  // Specify the end point's coordinates.
+  glVertex2f(1, 0);
+
+  // Do the same for the verticle line.
+  glVertex2f(0, -1);
+  glVertex2f(0, 1);
+
+  glEnd();
+}
+
+void drawParticle2D(const Particle& particle) {
+  float x, y;
+  float radius = 0.005 * particle.mass();
+  //radius = 0.001;
+
+  x = 2 * particle.x() / 4 - 1;
+  y = 2 * particle.y() / 4 - 1;
+
+  glBegin(GL_TRIANGLE_FAN);
+  glColor3f(0.0f, 0.67f, 1.0f);
+  glVertex2f(x, y);
+
+  for (int k = 0; k < 20; k++) {
+    float angle = (float) k / 19 * 2 * 3.141592;
+    glVertex2f(x + radius * cos(angle), y + radius * sin(angle));
+  }
+  glEnd();
 }
--- lab5/src/main.h
+++ lab5/src/main.h
@@ -6,6 +6,19 @@
 #include <string>
 #include <vector>
 
-void seq_barnes_hut(const Args& args);
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+void seq_barnes_hut(const Args& args, GLFWwindow *window);
 std::vector<Particle> read_particles(const std::string& filename);
 void write_particles(const std::string& filename, const std::vector<Particle>& particles);
+
+GLFWwindow *init_window();
+void draw(GLFWwindow *window, const std::vector<Particle>& particles);
+void drawOctreeBounds2D();
+void drawParticle2D(const Particle& particle);
