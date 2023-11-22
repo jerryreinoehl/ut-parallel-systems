7f2e44985d1613683ad9490390dece035cffb547
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-11-22 15:13:51 -0600

    Isolate visualization code
    Codio doesn't have OpenGL installed by default so this may fail to compile.

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -10,6 +10,10 @@
 
 #include <chrono>
 
+#ifdef VISUAL
+#include "visual.h"
+#endif
+
 int main(int argc, char **argv) {
   Args args{argc, argv};
 
@@ -29,11 +33,14 @@ void seq_barnes_hut(const Args& args) {
   double threshold = args.threshold();
   double timestep = args.timestep();
   double rlimit = args.rlimit();
+
+#ifdef VISUAL
   GLFWwindow *window{};
 
   if (args.visual()) {
     window = init_window();
   }
+#endif
 
   SpatialPartitionTree2D spt{size};
   std::vector<Particle> particles = read_particles(args.input());
@@ -42,13 +49,15 @@ void seq_barnes_hut(const Args& args) {
   auto start = std::chrono::high_resolution_clock::now();
 
   for (int step = 0; step < args.steps(); step++) {
-    if (args.visual()) {
-      draw(window, particles, spt);
-    }
-
     spt.reset();
     spt.put(particles);
     spt.compute_centers();
+
+#ifdef VISUAL
+    if (args.visual()) {
+      draw(window, particles, spt);
+    }
+#endif
 
     for (auto& particle : particles) {
       if (!spt.in_bounds(particle)) {
@@ -72,7 +81,10 @@ void mpi_barnes_hut(const Args& args) {
   int rank = mpi::rank();
   int num_procs = mpi::size();
   mpi::MessageGroup mg;
+
+#ifdef VISUAL
   GLFWwindow *window{};
+#endif
 
   const double size = 4;
   double gravity = args.gravity();
@@ -87,11 +99,13 @@ void mpi_barnes_hut(const Args& args) {
   int part_start, part_end;
   Vector2D force;
 
+#ifdef VISUAL
   bool visualize = rank == 0 && args.visual();
 
   if (visualize) {
     window = init_window();
   }
+#endif
 
   // First process reads particles and broadcasts to others.
   if (rank == 0) {
@@ -114,13 +128,15 @@ void mpi_barnes_hut(const Args& args) {
   auto start = std::chrono::high_resolution_clock::now();
 
   for (int step = 0; step < args.steps(); step++) {
-    if (visualize) {
-      draw(window, particles, spt);
-    }
-
     spt.reset();
     spt.put(particles);
     spt.compute_centers();
+
+#ifdef VISUAL
+    if (visualize) {
+      draw(window, particles, spt);
+    }
+#endif
 
     for (int i = part_start; i < part_end; i++) {
       particle = &particles[i];
@@ -186,101 +202,4 @@ void write_particles(const std::string& filename, const std::vector<Particle>& p
     out << particle.id() << '\t' << particle.x() << '\t' << particle.y() << '\t'
         << particle.mass() << '\t' << particle.dx() << '\t' << particle.dy() << '\n';
   }
-}
-
-GLFWwindow *init_window() {
-  const int width = 800, height = 800;
-  GLFWwindow *window;
-
-  if (!glfwInit()) {
-    fprintf(stderr, "Failed to initialize GLFW\n");
-    return nullptr;
-  }
-
-  // Open a window and create is OpenGL context.
-  window = glfwCreateWindow(width, height, "Simulation", NULL, NULL);
-  if (window == NULL) {
-    fprintf(stderr, "Failed to open GLFW window.\n");
-    glfwTerminate();
-    return nullptr;
-  }
-
-  glfwMakeContextCurrent(window); // Initialize GLEW
-  if (glewInit() != GLEW_OK) {
-    fprintf(stderr, "Failed to initialize GLEW\n");
-    return nullptr;
-  }
-
-  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-  return window;
-}
-
-void draw(GLFWwindow *window, const std::vector<Particle>& particles, const SpatialPartitionTree2D& spt) {
-  glClear(GL_COLOR_BUFFER_BIT);
-  drawOctreeBounds2D(spt);
-
-  for (const auto& particle : particles) {
-    drawParticle2D(particle);
-  }
-
-  // Swap buffers
-  glfwSwapBuffers(window);
-  glfwPollEvents();
-}
-
-void drawOctreeBounds2D(const SpatialPartitionTree2D& spt) {
-  double x1, y1, x2, y2;
-
-  for (const auto& bounds : spt.bounds()) {
-    glBegin(GL_LINES);
-
-    // Set the color of lines to be white.
-    glColor3f(1.0f, 1.0f, 1.0f);
-
-    x1 = bounds.x();
-    y1 = bounds.y();
-    x2 = x1 + bounds.z();
-    y2 = y1 + bounds.z();
-
-    // Transform bounds to fit on grid (-1, 1).
-    x1 = x1 / 2 - 1;
-    x2 = x2 / 2 - 1;
-    y1 = y1 / 2 - 1;
-    y2 = y2 / 2 - 1;
-
-    glVertex2f(x1, y1);
-    glVertex2f(x1, y2);
-
-    glVertex2f(x1, y2);
-    glVertex2f(x2, y2);
-
-    glVertex2f(x2, y2);
-    glVertex2f(x2, y1);
-
-    glVertex2f(x2, y1);
-    glVertex2f(x1, y1);
-
-    glEnd();
-  }
-}
-
-void drawParticle2D(const Particle& particle) {
-  float x, y;
-  float radius = 0.002 * particle.mass();
-  if (radius < 0.005) {
-    radius = 0.005;
-  }
-
-  x = particle.x() / 2 - 1;
-  y = particle.y() / 2 - 1;
-
-  glBegin(GL_TRIANGLE_FAN);
-  glColor3f(0.0f, 0.67f, 1.0f);
-  glVertex2f(x, y);
-
-  for (int k = 0; k < 20; k++) {
-    float angle = (float) k / 19 * 2 * 3.141592;
-    glVertex2f(x + radius * cos(angle), y + radius * sin(angle));
-  }
-  glEnd();
 }
--- lab5/src/main.h
+++ lab5/src/main.h
@@ -7,14 +7,6 @@
 #include <string>
 #include <vector>
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <GL/glu.h>
-#include <GL/glut.h>
-
 #include <mpi.h>
 #include "mpi.h"
 
@@ -23,8 +15,3 @@ void mpi_barnes_hut(const Args& args);
 
 std::vector<Particle> read_particles(const std::string& filename);
 void write_particles(const std::string& filename, const std::vector<Particle>& particles);
-
-GLFWwindow *init_window();
-void draw(GLFWwindow *window, const std::vector<Particle>& particles, const SpatialPartitionTree2D& spt);
-void drawOctreeBounds2D(const SpatialPartitionTree2D& spt);
-void drawParticle2D(const Particle& particle);
